2ac4ac794a0a2bf95d7763e20895719cf6e9fa54
Yang Zhang <yang.zhang@icloud.com>
2019-03-24 16:04:39 +1100

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,71 @@
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const AWS = require('aws-sdk');
+
+const STOCKS_TABLE = process.env.STOCKS_TABLE;
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+app.use(bodyParser.json({ strict: false }));
+
+app.get('/', function (req, res) {
+  res.send('Health check pass!')
+});
+
+// Get Stock endpoint
+app.get('/stock/:symbol', function (req, res) {
+  const params = {
+    TableName: STOCKS_TABLE,
+    Key: {
+      symbol: req.params.symbol,
+    },
+  }
+
+  dynamoDb.get(params, (error, result) => {
+    if (error) {
+      console.log(error);
+      res.status(400).json({ error: 'Could not get stock' });
+    }
+    if (result.Item) {
+      const {symbol, market, currency, description} = result.Item;
+      res.json({ symbol, market, currency, description });
+    } else {
+      res.status(404).json({ error: "Stock not found" });
+    }
+  });
+});
+
+// Create Stock endpoint
+app.post('/stock', function (req, res) {
+  const { symbol, market, currency, description } = req.body;
+  if (typeof symbol !== 'string') {
+    res.status(400).json({ error: '"symbol" must be a string' });
+  } else if (typeof market !== 'string') {
+    res.status(400).json({ error: '"market" must be a string' });
+  } else if (typeof currency !== 'string') {
+    res.status(400).json({ error: '"currency" must be a string' });
+  } else if (typeof description !== 'string') {
+    res.status(400).json({ error: '"description" must be a string' });
+  }
+
+  const params = {
+    TableName: STOCKS_TABLE,
+    Item: {
+      symbol: symbol,
+      market: market,
+      currency: currency,
+      description: description
+    },
+  };
+  
+  dynamoDb.put(params, (error) => {
+    if (error) {
+      console.log(error);
+      res.status(400).json({ error: 'Could not create stock due to db error.' });
+    }
+    res.json({ symbol });
+  });
+});
+
+module.exports.handler = serverless(app);
